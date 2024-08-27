24b3c6cce3bba2826d22b441d355a6b69e04192e
longgong1987 <longgong1987@gmail.com>
2024-08-27 15:44:28 +0700

    Add update and delete apis

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,22 +1,36 @@
+const fs = require('fs');
+const path = require('path');
 const  { validationResult } = require('express-validator');
 
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    Post.find()
+    const currentPage = req.query.page || 1;
+    const perPage = 2;
+    let totalItems;
+    Post.find().countDocuments()
+    .then(count => {
+        totalItems = count;
+        return Post.find()
+                    .skip((currentPage - 1) * perPage)
+                    .limit(perPage);
+    })
     .then(posts => {
         res.status(200)
         .json({
             message: 'Fetched posts successfully.',
-            posts: posts
+            posts: posts,
+            totalItems: totalItems
         });
     })
     .catch(err => {
         if (!err.statusCode) {
             err.statusCode = 500;
         }
-        next
+        next(err);
     });
+
+    
 }
 
 exports.createPost = (req, res, next) => {
@@ -26,6 +40,13 @@ exports.createPost = (req, res, next) => {
         error.statusCode = 422;
         throw error;
     }
+    if (!req.file) {
+        const error = new Error('No image provided.');
+        error.statusCode = 422;
+        throw error;
+    }
+
+    const imageUrl = req.file.path;
     const title = req.body.title;
     const content = req.body.content;
 
@@ -33,7 +54,7 @@ exports.createPost = (req, res, next) => {
     const post = new Post({
         title: title,
         content: content,
-        imageUrl: 'images/la.png',
+        imageUrl: imageUrl,
         creator: {
             name: 'ToRo'
         },
@@ -79,4 +100,90 @@ exports.getPostById = (req, res, next) => {
         }
         next(err);
     });
+}
+
+exports.updatePost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed, entered data is incorrect.');
+        error.statusCode = 422;
+        throw error;
+    }
+
+    const postId = req.params.postId;
+    const title = req.body.title;
+    const content = req.body.content;
+    let imageUrl = req.body.image;
+    if (req.file) {
+        imageUrl = req.file.path;
+    }
+    if (!imageUrl) {
+        const error = new Error('No image.');
+        error.statusCode = 422;
+        throw error;
+    }
+
+    Post.findById(postId)
+    .then(post => {
+        if (!post) {
+            const error = new Error('Could not find post.');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        // Clear old image is new image is uploaded
+        if (imageUrl !== post.imageUrl) {
+            clearImage(post.imageUrl);
+        }
+
+        post.title = title;
+        post.content = content;
+        post.imageUrl = imageUrl;
+        return post.save();
+    })
+    .then(result => {
+        res.status(200)
+        .json({
+            message: 'Post updated!',
+            post: result
+        });
+    })
+    .catch(err => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+}
+
+exports.deletePost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+    .then(post => {
+        if (!post) {
+            const error = new Error('Could not find post.');
+            error.statusCode = 404;
+            throw error;
+        }
+        // clear image
+        clearImage(post.imageUrl);
+        return Post.findByIdAndDelete(postId);
+    })
+    .then(result => {
+        // console.log(result);
+        res.status(200).json({
+            message: 'Deleted post.'
+        });
+    })
+    .catch(err => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+}
+
+const clearImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err => console.log(err));
 }
